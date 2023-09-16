86dab1f701e70cfc3f3927f1a2c890c548b520e5
EDWAR NEWGATE <136027194+EduardoMoraes2106@users.noreply.github.com>
2023-09-15 22:56:56 -0300

    Add files via upload

---
+++ nada.js
@@ -0,0 +1,9 @@
+function enviar() {
+
+    location.href = 'https://youtu.be/vwxnm4D4XuI?si=0vaZcHgCHMHr7ExZ&t=65'
+
+    let emoji = "\u{1F60E}"
+    let mensagem = 'cadrasto realizado' + emoji
+
+    alert(mensagem)
+}
